209b14a89a9db26829a64f347f3b0205818f1c11
perampokgoogle <localhost367@gmail.com>
2017-05-14 02:20:36 +0700

    Create onloaddisqusrampok.js

---
+++ onloaddisqusrampok.js
@@ -0,0 +1,8 @@
+    var disqus_shortname = &#39;perampokgoogle&#39;;
+    (function () {
+        var s = document.createElement(&#39;script&#39;); s.async = true;
+        s.type = &#39;text/javascript&#39;;
+        s.src = &#39;//&#39; + disqus_shortname + &#39;perampokgoogle.disqus.com/count.js&#39;;
+        (document.getElementsByTagName(&#39;HEAD&#39;)[0] || document.getElementsByTagName(&#39;BODY&#39;)[0]).appendChild(s);
+    }());
+}
