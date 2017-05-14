77f44b968c339f17be53d5c4b246659e43d52e3f
perampokgoogle <localhost367@gmail.com>
2017-05-14 13:31:57 +0700

    Create blockdisqus.js

---
+++ blockdisqus.js
@@ -0,0 +1,31 @@
+var mql = window.matchMedia(&#39;screen and (min-width: 600px)&#39;);if (mql.matches){
+                var disqus_shortname = &quot;perampokgoogle&quot;;
+                var disqus_blogger_current_url = &quot;<data:blog.canonicalUrl/>&quot;;
+                if (!disqus_blogger_current_url.length) {
+                    disqus_blogger_current_url = &quot;<data:blog.url/>&quot;;
+                }
+                var disqus_blogger_homepage_url = &quot;<data:blog.homepageUrl/>&quot;;
+                var disqus_blogger_canonical_homepage_url = &quot;<data:blog.canonicalHomepageUrl/>&quot;;
+             (function() {
+                        var bloggerjs = document.createElement(&quot;script&quot;);
+                        bloggerjs.type = &quot;text/javascript&quot;;
+                        bloggerjs.async = true;
+                        bloggerjs.src = &quot;//&quot;+disqus_shortname+&quot;.disqus.com/blogger_item.js&quot;;
+                        (document.getElementsByTagName(&quot;head&quot;)[0] || document.getElementsByTagName(&quot;body&quot;)[0]).appendChild(bloggerjs);
+                    })();
+                (function() {
+                    var bloggerjs = document.createElement(&quot;script&quot;);
+                    bloggerjs.type = &quot;text/javascript&quot;;
+                    bloggerjs.async = true;
+                    bloggerjs.src = &quot;//&quot;+disqus_shortname+&quot;.disqus.com/blogger_index.js&quot;;
+                    (document.getElementsByTagName(&quot;head&quot;)[0] || document.getElementsByTagName(&quot;body&quot;)[0]).appendChild(bloggerjs);
+                })();
+
+    var disqus_shortname = &#39;perampokgoogle&#39;;
+    (function () {
+        var s = document.createElement(&#39;script&#39;); s.async = true;
+        s.type = &#39;text/javascript&#39;;
+        s.src = &#39;//&#39; + disqus_shortname + &#39;perampokgoogle.disqus.com/count.js&#39;;
+        (document.getElementsByTagName(&#39;HEAD&#39;)[0] || document.getElementsByTagName(&#39;BODY&#39;)[0]).appendChild(s);
+    }());
+}
